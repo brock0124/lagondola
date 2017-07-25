5c08e4760474bcf143330baa3dcb3483d64938a9
BuildTools <unconfigured@null.spigotmc.org>
2017-07-25 14:11:22 -0500

    Got zend-mail working both dev and prod

--- src/App/src/Action/ContactAction.php
+++ src/App/src/Action/ContactAction.php
@@ -19,6 +19,9 @@ use Zend\Expressive\Plates\PlatesRenderer;
 use Zend\Expressive\Twig\TwigRenderer;
 use Zend\Expressive\ZendView\ZendViewRenderer;
 use Zend\Mail;
+use Zend\Mail\Transport\Smtp as SmtpTransport;
+use Zend\Mail\Transport\SmtpOptions;
+use Zend\Mime\Mime;
 
 class ContactAction implements ServerMiddlewareInterface
 {
@@ -28,16 +31,57 @@ class ContactAction implements ServerMiddlewareInterface
 
     public function __construct(Router\RouterInterface $router, Template\TemplateRendererInterface $template = null)
     {
-        $this->router   = $router;
+        $this->router = $router;
         $this->template = $template;
     }
 
     public function process(ServerRequestInterface $request, DelegateInterface $delegate)
     {
+        $data = $request->getParsedBody();
+        $transport = new SmtpTransport();
+
+        $endOfMessage = $data['message'] . '<br><br>Please do no reply to this message.  Reply by copying and 
+pasting the email address below into a new email. Not creating a new message will not send it to the customer.
+<br>' . $data['email'];
+
+        $bodyPart = new \Zend\Mime\Message();
+
+        $bodyMessage = new \Zend\Mime\Part($endOfMessage);
+        $bodyMessage->type = 'text/html';
+
+        $bodyPart->setParts(array($bodyMessage));
+
+
+        //Developer SMTP Options
+        /*$options = new SmtpOptions([
+            'name' => 'srv-16.caldwell.local',
+            'host' => '192.168.1.17',
+            'port' => 1025
+        ]);*/
+
+        //Production SMTP Options
+        /*$options = new SmtpOptions([
+            'name' => 'gmail.com',
+            'host' => 'smtp.gmail.com',
+            'connection_class' => 'login',
+            'port' => 465,
+            'connection_config' => [
+                'username' => 'lagondolaspaghettihouse@gmail.com',
+                //RE-INSERT PASSWORD BEFORE BEGINNING
+                'ssl' => 'ssl',
+            ],
+        ]);*/
+
+        $transport->setOptions($options);
 
         $message = new Mail\Message();
-        $data = $request->getParsedBody();
-        die();
+
+        $message->setBody($bodyPart);
+        $message->setFrom($data['email']);
+        $message->addTo('lagondolaspaghettihouse@gmail.com');
+        $message->setSubject($data['location'] . " - Customer Email");
+        $message->addReplyTo($data['email']);
+        $transport->send($message);
 
 
         return new HtmlResponse($this->template->render('app::index', $data));
